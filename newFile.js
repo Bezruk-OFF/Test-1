ba3e6804c6adac6c626eb11ce655627e98b6869e
Ivan Bezrukov <van-84@yandex.ru>
2022-07-27 21:40:20 +0700

    commit 6 vs code

--- newFile.js
+++ newFile.js
@@ -10,9 +10,11 @@ if (2 > 1) {
 }
 
 let arr = [];
-for (let i = 1; i <= 50; i++) {
+for (let i = 1; i <= 25; i++) {
     pages.push(i);
 }
+
+console.log(arr);
 
 function get2dimensional(array, limit) {             // Делаем из array двумерный массив get2dimensional
     const result = [];
